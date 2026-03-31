71dc4df1522ca877a7d0f568e28f2a80c094ff8a
ldonald1 <lelad12550@gmail.com>
2026-03-31 13:04:28 -0400

    Add color change functionality
    Implemented grid divs changing color when your
    mouse passes over them.

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 let divsArray = [];
 
 const divContainer = document.querySelector(".gridContainer");
+const newGrid = document.querySelector("#new");
 
-
-for(let i = 0; i < 16; i ++){
-    for(let j = 0; j < 16; j++){
+function generateGrid(n){
+    for(let i = 0; i < n; i ++){
+    for(let j = 0; j < n; j++){
     const div = document.createElement("div");
     div.id = `div${i}-${j}`;
     div.classList.add("gridItem");
+    
     divsArray.push(div);
     divContainer.appendChild(div);
+        }
     }
 }
+
+generateGrid(16);
+
+divContainer.addEventListener("mouseover", (e) => {
+        const chosen = e.target;
+        console.log(chosen);
+        if(chosen.hasChildNodes()){return;}
+        chosen.style.backgroundColor = "blue";
+    })
+
+newGrid.addEventListener("click", () => {
+    const input = +prompt("How many squares per side do you want for your new grid?");
+
+})
+
