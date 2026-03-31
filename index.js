9bfdee49542cac1ed625a6d60d606231af52356a
ldonald1 <lelad12550@gmail.com>
2026-03-31 14:58:59 -0400

    Clean up code
    Put repetitive code into function drawColor().

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const divContainer = document.querySelector(".gridContainer");
 const newGrid = document.querySelector("#new");
 const colorChoices = document.querySelector("#choices");
 
-function generateGrid(n){
+function generateGrid(n){ // generate grid of size n x n
     for(let i = 0; i < n; i ++){
         for(let j = 0; j < n; j++){
         const div = document.createElement("div");
@@ -20,41 +20,43 @@ function generateGrid(n){
     }
 }
 
-function removeGrid(){
+function removeGrid(){ // remove the grid and clear array and colorChoice
     divsArray.forEach((e) => e.remove());
     divsArray.length = 0;
     colorChoice = "";
 };
 
-function randomizeRGB(){
+function randomizeRGB(){ // generate random RGB value
     let rgb1 = Math.floor(Math.random() * 256);
     let rgb2 = Math.floor(Math.random() * 256);
     let rgb3 = Math.floor(Math.random() * 256);
     return `(${rgb1}, ${rgb2}, ${rgb3})`;
 }
 
+function drawColor(square){ // change color of square to colorChoice
 
-generateGrid(16);
+    if(colorChoice === "random"){
+    square.style.backgroundColor = `rgb${randomizeRGB()}`;
+    }
+    else{square.style.backgroundColor = colorChoice;}
+}
 
 
-divContainer.addEventListener("mouseover", (e) => {
+generateGrid(16); // generate initial grid
+
+
+divContainer.addEventListener("mouseover", (e) => { // color square div when mouse hovers over it
         const chosen = e.target;
         console.log(chosen);
-        if(chosen.hasChildNodes()){return;}
+        if(chosen.hasChildNodes()){return;} // make sure target is not divContainer
         
         if(chosen.style.backgroundColor === "white"){
-            if(colorChoice === "random"){
-            chosen.style.backgroundColor = `rgb${randomizeRGB()}`;
-            }
-            else{chosen.style.backgroundColor = colorChoice;}
+            drawColor(chosen);
             return;
         }
 
         if(chosen.style.opacity>0){
-        if(colorChoice === "random"){
-        chosen.style.backgroundColor = `rgb${randomizeRGB()}`; 
-        }
-        else{chosen.style.backgroundColor = colorChoice;}   
+        drawColor(chosen);   
         chosen.style.opacity-= 0.1
         }
         
