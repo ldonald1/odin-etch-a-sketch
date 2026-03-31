c7c479efc42eb9119a140f56b3d8ca0e44b5d3d7
ldonald1 <lelad12550@gmail.com>
2026-03-31 13:25:59 -0400

    Implement new grid button
    Implemented a button which allows you to make a
    new grid of size nxn.

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const newGrid = document.querySelector("#new");
 
 function generateGrid(n){
     for(let i = 0; i < n; i ++){
-    for(let j = 0; j < n; j++){
-    const div = document.createElement("div");
-    div.id = `div${i}-${j}`;
-    div.classList.add("gridItem");
-    
-    divsArray.push(div);
-    divContainer.appendChild(div);
+        for(let j = 0; j < n; j++){
+        const div = document.createElement("div");
+        div.id = `div${i}-${j}`;
+        div.classList.add("gridItem");
+        div.style.width = (100/n)+"%";
+        divsArray.push(div);
+        divContainer.appendChild(div);
         }
     }
 }
+
+function removeGrid(){
+    divsArray.forEach((e) => e.remove());
+};
 
 generateGrid(16);
 
@@ -27,6 +31,12 @@ divContainer.addEventListener("mouseover", (e) => {
 
 newGrid.addEventListener("click", () => {
     const input = +prompt("How many squares per side do you want for your new grid?");
-
+    if(input < 0 || input > 100){
+        alert("The number can not be negative or above 100.");
+        return;
+    }
+    removeGrid();
+    divsArray.length = 0;
+    generateGrid(input);
 })
 
